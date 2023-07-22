d451adc1c1c4245ff5c5c2375b2bf2111d3f9890
Mahdi Namaki <mavenium@gmail.com>
2023-07-22 09:57:11 +0330

    Add : the app.py file for backend servers section

---
+++ backend_servers/src/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+from os import environ
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    return f'This is the {environ["APP"]} application.'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
