a4ab08904191449ee7a82af67aa9e97847fa9cf8
Mahdi Namaki <mavenium@gmail.com>
2023-07-23 18:17:01 +0330

    Fix : Fix the 'app.py' script

--- backend_servers/src/app.py
+++ backend_servers/src/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return f'This is the {environ["APP"]} application.'
+    return f'This is the {environ["APP"]} application.\n'
 
 
 if __name__ == '__main__':
