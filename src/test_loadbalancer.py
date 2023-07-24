0a1010f507579b8ab226e99eff30f4829d6243e0
Mahdi Namaki <mavenium@gmail.com>
2023-07-24 11:13:00 +0330

    Add : Add the 'IntegrationTests' test class

--- src/test_loadbalancer.py
+++ src/test_loadbalancer.py
@@ -1,9 +1,17 @@
+from http.server import HTTPServer
+
+from threading import Thread
+
+from time import sleep
+
 from unittest.mock import patch, Mock
 import unittest
 
 from LoadBalancer import LoadBalancerHandler
 
 from io import BytesIO as IO
+
+import requests
 
 
 class LoadBalancerTest(unittest.TestCase):
@@ -22,6 +30,8 @@ class LoadBalancerTest(unittest.TestCase):
         handler.backend_servers = self.__backend_servers
         handler.current_server_id = 0
         server = handler.get_backend_server()
+
+        # Asserts
         self.assertEqual(server, {"host": "localhost", "port": 8081})
 
     def test_move_to_next_backend_server(self):
@@ -31,6 +41,8 @@ class LoadBalancerTest(unittest.TestCase):
         handler.current_server_id = 0
         handler.move_to_next_backend_server()
         server = handler.get_backend_server()
+
+        # Asserts
         self.assertEqual(server, {"host": "localhost", "port": 8082})
         self.assertEqual(handler.current_server_id, 1)
 
@@ -42,8 +54,35 @@ class LoadBalancerTest(unittest.TestCase):
         mock_do_get.return_value = "/"
         mock_request.makefile.return_value = IO(b"GET /")
         server = LoadBalancerHandler(mock_request, ('localhost', 8080), mock_http_server)
+
+        # Asserts
         self.assertTrue(mock_do_get.called)
         self.assertEqual(server.do_GET(), "/")
+
+
+class IntegrationTests(unittest.TestCase):
+    def setUp(self):
+        # Start the load balancer server
+        self.host = "localhost"
+        self.port = 8090
+        self.server = HTTPServer((self.host, self.port), LoadBalancerHandler)
+        self.server_thread = Thread(target=self.server.serve_forever)
+        self.server_thread.start()
+
+        # Wait for the server to start
+        sleep(1)
+
+    def tearDown(self):
+        # Stop the load balancer server
+        self.server.shutdown()
+        self.server_thread.join()
+
+    def test_load_balancer(self):
+        # Send a GET request to the load balancer
+        response = requests.get(f"http://{self.host}:{self.port}")
+
+        # Assert that the response is successful (200)
+        self.assertEqual(response.status_code, 200)
 
 
 if __name__ == "__main__":
